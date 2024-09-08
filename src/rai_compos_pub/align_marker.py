45e7e81de35a14635390a086c2a01e4d56050884
maybetree <maybetree48@proton.me>
2024-09-08 19:54:25 +0200

    Add annotations to alignment marker

--- src/rai_compos_pub/align_marker.py
+++ src/rai_compos_pub/align_marker.py
@@ -21,9 +21,26 @@ class HalfAlignMarker(rai.Compo):
 
 
 class AlignMarker(rai.Compo):
+    """
+    This is another example component inspired by Leon's work with
+    alignment markers and test patterns.
+    """
+
     class Layers:
         left = rai.Layer('Left side of marker')
         right = rai.Layer('Right side of marker')
+
+    class Options:
+        num_rects = rai.Option.Geometric(
+            "Number of rectangles on each side")
+        rect_width = rai.Option.Geometric(
+            "Width of each rectangle on left side")
+        rect_length = rai.Option.Geometric(
+            "Length of each of rectangle on left side")
+        gap_width = rai.Option.Geometric(
+            "Width of gap between each rectangle on left side")
+        abberation = rai.Option.Geometric(
+            "Scale of right side compared to left (a value of 1 = same size)")
 
     def _make(
             self,
