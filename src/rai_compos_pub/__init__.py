bff8a87e42a8c3e735da11d7227d40a2a0cd4bde
maybetree <maybetree48@proton.me>
2024-11-29 13:50:41 +0000

    Add testpattern

--- src/rai_compos_pub/__init__.py
+++ src/rai_compos_pub/__init__.py
@@ -1,3 +1,4 @@
 from rai_compos_pub.smiley import Smiley
 from rai_compos_pub.test_pattern import TestPattern
+from rai_compos_pub.test_pattern import TestPatterns
 
--- src/rai_compos_pub/test_pattern.py
+++ src/rai_compos_pub/test_pattern.py
@@ -57,17 +57,46 @@ class TestPattern(rai.Compo):
             rect_width,
             rect_length,
             gap_width
-            ).proxy().map('left')
+            ).proxy()
         self.subcompos.right = (
             self.subcompos.left
             .proxy()
-            .map('right')
             .scale(abberation)
             .snap_right(
                 self.subcompos.left
                 )
             )
 
+class TestPatterns(rai.Compo):
+    """
+    Test Patterns
+
+    Test component consisting of multiple test patterns in differnet positions.
+    """
+
+
+    def _make(self):
+        pattern = TestPattern()
+        self.subcompos.append(pattern.proxy())
+        self.subcompos.append(pattern.proxy().move(-200, 0))
+        self.subcompos.append(pattern.proxy().move(200, 0))
+        self.subcompos.append(
+            pattern
+            .proxy()
+            .move(0, 400)
+            .bbox.mid.rotate(rai.quartercircle)
+            )
+
+        pattern2 = (
+            TestPattern(num_rects=5, rect_width=40)
+            .proxy()
+            .bbox.mid.rotate(rai.quartercircle)
+            )
+        self.subcompos.append(pattern2.proxy().move(-300, -400))
+        self.subcompos.append(pattern2.proxy().move(300, -400))
+
 if __name__ == '__main__':
-    rai.show(TestPattern())
+    with open('a.svg', 'w') as f:
+        f.write(TestPatterns()._repr_svg_())
+    rai.show(TestPatterns())
 
